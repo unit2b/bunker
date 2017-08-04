d599203921b93b9946290263ed23fc1d92d9f454
YoRHa No.2 Type B <unit.2b@yorha.army>
2017-08-05 04:46:55 +0800

    update scripts

--- derive.sh
+++ derive.sh
@@ -4,3 +4,7 @@ set -e
 set -u
 
 curl https://$BUNKER_USER:$BUNKER_PASS@$BUNKER_HOST$1 > /dev/null
+
+echo ""
+echo "-----"
+echo "https://$BUNKER_HOST$1"
--- upload.sh
+++ upload.sh
@@ -5,5 +5,6 @@ set -u
 
 curl -XPUT --data-binary @$1 https://$BUNKER_USER:$BUNKER_PASS@$BUNKER_HOST$2
 
-echo "\n-----"
+echo ""
+echo "-----"
 echo "https://$BUNKER_HOST$2"
